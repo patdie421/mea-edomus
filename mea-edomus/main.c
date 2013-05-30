510d295b271ef942c7006f05388ac2bfb0ba7be7
Patrice Dietsch <patrice@Elsass4.local>
2013-05-30 21:57:13 +0200

    Initial Commit

---
+++ mea-edomus/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  mea-edomus
+//
+//  Created by Patrice Dietsch on 30/05/13.
+//  Copyright (c) 2013 Patrice Dietsch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+   // insert code here...
+   printf("Hello, World!\n");
+    return 0;
+}
+
