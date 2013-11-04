6a4d4cfc1df7c6d73289551be89747a5cea6e528
Patrice Dietsch <patrice@elsass4.local>
2013-11-04 22:32:01 +0100

    ajout de interfaces.c

---
+++ src/interfaces.c
@@ -0,0 +1,85 @@
+//
+//  interfaces.c
+//  mea-edomus
+//
+//  Created by Patrice Dietsch on 04/11/2013.
+//
+//
+
+#include <stdio.h>
+#include <termios.h>
+#include <inttypes.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include "string_utils.h"
+
+
+uint32_t speeds[][3]={
+   {   300,    B300},
+   {  1200,   B1200},
+   {  4800,   B4800},
+   {  9600,   B9600},
+   { 19200,  B19200},
+   { 38400,  B38400},
+   { 57600,  B57600},
+   {115200, B115200},
+   {0,0}
+};
+
+
+int16_t get_dev_and_speed(char *device, char *dev, int16_t dev_l, speed_t *speed)
+{
+   *speed=0;
+   char _dev[41];
+   char reste[41];
+   char vitesse[41];
+
+   char *_dev_ptr;
+   char *reste_ptr;
+   char *vitesse_ptr;
+   char *end=NULL;
+
+   int16_t n=sscanf(device,"SERIAL://%40[^:]%40[^/r/n]",_dev,reste);
+   if(n<=0)
+      return -1;
+
+   _dev_ptr=trim(_dev);
+
+   if(n==1)
+   {
+      *speed=B9600;
+   }
+   else
+   {
+      uint32_t v;
+
+      reste_ptr=trim(reste);
+      n=sscanf(reste,":%40[^/n/r]",vitesse);
+      if(n!=1)
+         return -1;
+
+      vitesse_ptr=trim(vitesse);
+      v=strtol(vitesse_ptr,&end,10);
+      if(end==vitesse || *end!=0 || errno==ERANGE)
+         return -1;
+
+      *speed=0;
+      int i;
+      for(i=0;speeds[i][0];i++)
+      {
+         if(speeds[i][0]==v)
+         {
+            *speed=speeds[i][1];
+            break;
+         }
+      }
+      if(*speed==0)
+         return -1;
+   }
+
+   strncpy(dev,_dev_ptr,dev_l);
+
+   return 0;
+}
