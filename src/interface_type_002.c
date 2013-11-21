ed865b10cce73756acce357e41fd2037a8db8fde
Patrice Dietsch <patrice@elsass4.local>
2013-11-21 22:36:38 +0100

    debug

--- src/interface_type_002.c
+++ src/interface_type_002.c
@@ -1138,6 +1138,7 @@ mea_error_t start_interface_type_002(interface_type_002_t *i002, sqlite3 *db, in
          interface_nb_parameters=0;
       }
    }
+   sleep(10);
    exit(1);
    /*
     * parametrage du réseau
