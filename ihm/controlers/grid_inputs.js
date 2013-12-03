213a9c7c90349ca2bc626a067fa24b9163d1ff24
Patrice Dietsch <patrice@elsass4.local>
2013-12-03 22:26:10 +0100

    IHM : form custom pour la saisie

--- ihm/controlers/grid_inputs.js
+++ ihm/controlers/grid_inputs.js
@@ -1,6 +1,9 @@
 var inputs_inEdit;
 
 function grid_inputs() {
+
+   init_input_form();
+   
    jQuery("#table_inputs").jqGrid({
       url:'models/get_inputs-jqg_grid.php',
       datatype: "xml",
@@ -27,10 +30,14 @@ function grid_inputs() {
       gridComplete: function(){
          $("#table_inputs_edit").button('disable');
          $("#table_inputs_del").button('disable');
+         $("#table_inputs_edit_nav").addClass('ui-state-disabled');
+         $("#table_inputs_del_nav").addClass('ui-state-disabled');
       },
       onSelectRow: function(){
-         $("#table_interfaces_edit").button('enable');
-         $("#table_interfaces_del").button('enable');
+         $("#table_inputs_edit").button('enable');
+         $("#table_inputs_del").button('enable');
+         $("#table_inputs_edit_nav").removeClass('ui-state-disabled');
+         $("#table_inputs_del_nav").removeClass('ui-state-disabled');
       },
       loadError: function(xhr,st,err) {
          if(st=="parsererror") {
@@ -49,20 +56,222 @@ function grid_inputs() {
          else
             alert("Type: "+st+"; Response: "+ xhr.status + " "+xhr.statusText+" "+xhr.responseText);
       }
-});
+   });
     
    $("#table_inputs").jqGrid('navGrid',
       '#pager_inputs',
-      {search:false, edit:true, add:true, del:true },
-      {width:700, recreateForm: true, viewPagerButtons: false, beforeInitData: function () {inputs_inEdit = true; return check_auth(); },  closeAfterEdit: true, afterShowForm: function(){ $("#name").focus() } }, // edit option
+      {search:false, edit:false, add:false, del:false }
+/*
+      ,{width:700, recreateForm: true, viewPagerButtons: false, beforeInitData: function () {inputs_inEdit = true; return check_auth(); },  closeAfterEdit: true, afterShowForm: function(){ $("#name").focus() } }, // edit option
       {width:700, recreateForm: true, viewPagerButtons: false, beforeInitData: function () {interfaces_inEdit = false;return check_auth(); }, closeAfterAdd: true}, // add option
       {width:600}  // delete option
-    );
+*/
+   )
+   .navButtonAdd('#pager_inputs',
+      {  caption:"",
+         id:"table_inputs_del_nav",
+         buttonicon:"ui-icon-trash",
+         onClickButton: function(){
+            alert("Deleting Row"); },
+      position:"first"
+   })
+   .navButtonAdd('#pager_inputs',
+      {  id:"table_inputs_edit_nav",
+         caption:"",
+         buttonicon:"ui-icon-pencil",
+         onClickButton: function(){
+         alert("Editing Row"); },
+      position:"first"
+   })
+   .navButtonAdd('#pager_inputs',
+      {  caption:"",
+         buttonicon:"ui-icon-plus",
+         onClickButton: function(){
+         open_input_form(); },
+      position:"first"
+   })
+   ;
     
    $('#t_table_inputs').height(40);
-   add_button_to_toolbar('table_inputs',"add",str_add.capitalize(),function(){if(check_auth()){_addRow('table_inputs',function(){inputs_inEdit = false;})}},"ui-icon-plusthick");
+   add_button_to_toolbar('table_inputs',"add",str_add.capitalize(),function(){if(check_auth()){_addRow('table_inputs',function(){inputs_inEdit = false;})}},"ui-icon-plus");
    add_button_to_toolbar('table_inputs',"edit",str_edit.capitalize(),function(){if(check_auth()){_editRow('table_inputs',function(){inputs_inEdit = true;})}},"ui-icon-pencil");
    add_button_to_toolbar('table_inputs',"del",str_del.capitalize(),function(){delete_input();},"ui-icon-trash");
+   /*
    $("#table_inputs_edit").button('disable');
    $("#table_inputs_del").button('disable');
+   $("#table_inputs_edit_nav").addClass('ui-state-disabled');
+   $("#table_inputs_del_nav").addClass('ui-state-disabled');
+   */
+}
+
+function updateTips( t ) {
+   tips
+   .text( t )
+   .addClass( "ui-state-highlight" );
+   setTimeout(function() {
+      tips.removeClass( "ui-state-highlight", 1500 );
+   }, 500 );
+}
+
+function checkLength( o, n, min, max ) {
+   if ( o.val().length > max || o.val().length < min ) {
+      o.addClass( "ui-state-error" );
+      updateTips( "Length of " + n + " must be between " +
+      min + " and " + max + "." );
+      return false;
+   } else {
+      return true;
+   }
+}
+   
+function checkRegexp( o, regexp, n ) {
+   v = o.val();
+   $.trim(v);
+   if ( !( regexp.test( v ) ) ) {
+      o.addClass( "ui-state-error" );
+      updateTips( n );
+      return false;
+   } else {
+      return true;
+   }
+}
+
+function disable_keyvalue(event) {
+   $("#key2").attr("readonly", "readonly");
+   $("#key2").addClass( "mea_disabled" );
+}
+
+function enable_keyvalue(event) {
+   $("#key2").removeAttr("readonly");
+   $("#key2").removeClass( "mea_disabled" );
+   $("#key2").focus();
+}
+
+function init_input_form() {
+
+   var tips = $( "#tips" ),
+       _key = $( "#key" ),
+       _value = $( "#value" ),
+       _xplsource = $("#xplsource"),
+       _xplschema = $("#xplschema"),
+       _inputindex = $("#inputindex"),
+       _key2 = $("#key2");
+       
+       allFields = $( [] ).add( _key ).add( _value ).add( _xplsource ).add( _xplschema ).add( _inputindex ).add( _key2 );
+
+//    $.ajaxSetup({ cache: false });
+   
+   $("#true").click(disable_keyvalue);
+   $("#false").click(disable_keyvalue);
+   $("#keyvalue").click(enable_keyvalue);
+   
+   $("#selectable").selectable();
+   $("#del_condition").button().click(function( event ) { event.preventDefault(); $(".ui-selected","#selectable").remove(); });
+   $("#add_condition").button().click(function( event ) {
+      event.preventDefault();
+      
+      key=_key.val();
+      value=_value.val();
+      $.trim(key);
+      $.trim(value);
+      
+      _key.removeClass( "ui-state-error" );
+      _value.removeClass( "ui-state-error" );
+      
+      if(key=="") {
+         _key.addClass( "ui-state-error" );
+      }
+      if(value=="") {
+         _value.addClass( "ui-state-error" );
+      }
+      if(key!="" && value!="") {
+         b = checkRegexp( _key, /^[0-9a-z_]{1,16}$/, "'Value for key' may consist of a-z, 0-9 or underscores." );
+         if(b) {
+            if(($("#op").val() == '=') || ($("#op").val() == '!=') ) {
+               if(!$.isNumeric( _value.val() )) {
+                  value="\""+value+"\"";
+               }
+            }
+            else {
+               if(!$.isNumeric( _value.val() )) {
+                  _value.addClass( "ui-state-error" );
+                  updateTips("With '>', '>=', '<' and '<=' operators, 'A value' must be numeric.");
+                  b = false;
+               }
+            }
+         }
+         if(b) {
+            $("#key").val("");
+            $("#value").val("");
+            keyvalue=key+" "+$("#op").val()+" "+value;
+            x="<li>"+keyvalue+"</li>";
+            $("#selectable").append(x);
+         }
+      }
+      else {
+         updateTips("'Value for key' and 'A value' can't be empty !");
+      }
+      
+   });
+ 
+   $( "#dialog-form" ).dialog({
+      autoOpen: false,
+      height: 600,
+      width: 631,
+      modal: true,
+      resizable: false,
+      buttons: {
+         "Add": function() {
+            allFields.removeClass( "ui-state-error" );
+            b = checkRegexp( _xplsource, /^[0-9a-z]{1,8}-[0-9a-z]{1,8}\.[0-9a-z\-]{1,16}$/, "xlp address format error (VendorID-DeviceID.InstanceID)" );
+            if(!b)
+               return;
+            b = checkRegexp( _xplschema, /^[0-9a-z]{1,8}\.[0-9a-z]{1,8}$/, "xlp schema format error ()" );
+            if(!b)
+               return;
+            b = checkRegexp( _inputindex, /^[0-9]+$/, "must be numeric (0 <= input index <= 255)" );
+            if(!b)
+               return;
+            v = parseInt($.trim(_inputindex.val()));
+            if(v > 255) {
+               updateTips("must be numeric (0 <= input index <= 255)");
+               _inputindex.addClass( "ui-state-error" );
+               return;
+            }
+            if($("#selectable").children().length == 0) {
+               updateTips("At least one condition is needed");
+               return;
+            }
+            if($("#keyvalue:checked").val()==3) {
+               b = checkRegexp( _key2, /^[0-9a-z_]{1,16}$/, "'Value from body key' may consist of a-z, 0-9 or underscores." );
+               if(!b)
+                  return;
+            }
+            
+            // recolter les données et le transmettre pour mise à jour
+            
+            $( this ).dialog( "close" );
+         },
+         Cancel: function() {
+            $( this ).dialog( "close" );
+         }
+      },
+      close: function() {
+         // allFields.val( "" ).removeClass( "ui-state-error" );
+      }
+   });
+}
+
+function open_input_form() {
+   allFields.removeClass( "ui-state-error" ).val( "" ).on( "focus", function() { $(this).removeClass( "ui-state-error" ); } );
+   disable_keyvalue();
+   $("#true").attr('checked','checked');
+   $("#selectable li").remove();
+
+   var myGrid = $("#table_inputs"),
+   selRowId = myGrid.jqGrid ('getGridParam', 'selrow'),
+   celValue = myGrid.jqGrid ('getCell', selRowId, 'name');
+
+   $("#xplsource").val(celValue);
+   $( "#dialog-form" ).dialog( "open" );
 }
--- ihm/views/automate-contenu.php
+++ ihm/views/automate-contenu.php
@@ -37,3 +37,141 @@ jQuery(document).ready(function(){
    grid_inputs();
 });
 </script>
+
+
+<style>
+   .ui-widget{font-size:14px;}
+
+   #feedback { font-size: 1em; }
+   #selectable .ui-selecting { background: #FECA40; }
+   #selectable .ui-selected { background: #F39814; color: white; }
+   #selectable { list-style-type: none; margin: 0; padding: 0; }
+   #selectable li { padding: 0.4em; font-size: 1em; height: 1em; }
+
+//   label, input { display:block; }
+   label { font-size:0.8em; }
+   input.text { text-align:center; margin-bottom:12px; width:100%; padding-top:0.4em; padding-bottom:0.4em; }
+   fieldset { padding:0; border:0; margin-top:25px; }
+
+   .ui-dialog { padding: .3em; }
+   .validateTips { border: 1px solid transparent; padding-bottom: 0.3em; }
+   .mea_disabled { color: #b9b9b9; background: #E0E0E0; }
+</style>
+
+
+<div id="dialog-form" title="Règle">
+   <div id="tips" class="validateTips">
+      Associer un message xPL à une entrée de l'automate.
+   </div>
+
+   <label for="key" style="font-size:0.9em;">Conditions :</label>
+
+   <div class="ui-widget-content" style="padding:15px;">
+      <table>
+         <tr>
+         <td width="265px">
+            <label>xPL message from (source address) :</label>
+            <input id="xplsource" name="xplsource" style="width:265px" class="text ui-widget-content ui-corner-all">
+         </td>
+         <td width="24px">
+         </td>
+         <td width="265px">
+            <label>With xPL Schema :</label>
+            <input id="xplschema" name="xplschema" style="width:265px" class="text ui-widget-content ui-corner-all">
+         </td>
+         </tr>
+      </table>
+
+      <table>
+         <tr>
+         <td valign="top" align="center" width="220px">
+            <div>
+               <label style="text-align:left;">xPL body key/value condition :</label>
+               <div class="ui-widget-content" style="display:table;">
+                  <div style="display:table-cell; vertical-align: middle; padding:5px">
+                     <label for="key" style="text-align:left;">Value for key :</label>
+                     <input id="key" name="key" style="width:205px" class="text ui-widget-content ui-corner-all">
+                     <div style="text-align:center">
+                        <select id="op" class="ui-widget-content ui-corner-all" style="margin-bottom:5px;">
+                           <option>=</option>
+                           <option>!=</option>
+                           <option>></option>
+                           <option>=></option>
+                           <option><</option>
+                           <option>=<</option>
+                        </select>
+                     </div>
+                     <label for="value" style="text-align:left;">A value (string or numeric) :</label>
+                     <input id="value" name="value" style="width:205px" class="text ui-widget-content ui-corner-all">
+                  </div>
+               </div>
+            </div>
+         </td>
+
+         <td width="105px">
+            <div style="display:table; vertical-align: middle; align:center; width:100px;">
+               <div style="display:table-cell;">
+                  <button id="add_condition" style="width:60px; display:block; margin:5px; margin-left:auto; margin-right:auto;">add</button>
+                  <button id="del_condition" style="width:60px; display:block; margin:5px; margin-left:auto; margin-right:auto;">del</button>
+               </div>
+            </div>
+         </td>
+
+         <td width="220px">
+            <label>xPL body conditions list :</label>
+            <div class="ui-widget-content" style="width:230px; height:150px; overflow:auto">
+               <ol id="selectable" style="text-align:center">
+               </ol>
+            </div>
+         </td>
+         </tr>
+      </table>
+
+   </div>
+   <p></p>
+   <label for="key" style="font-size:0.9em;">Set :</label>
+
+   <div class="ui-widget-content" style="padding:10px; padding-down:5px">
+      <table>
+         <tr>
+
+         <td width="205px" valign="top">
+            <label>Input type :</label>
+            <div id="radio1" style="margin-right:5px; padding:5px; text-align:center;" class="ui-widget-content ui-corner-all ">
+               <input type="radio" class="picker" id="digital" name="radio1" checked='true' style="display:inline;"/>
+               <label for="digital" style="display:inline;">Digital</label>
+               <input type="radio" class="picker" id="analog" name="radio1" style="display:inline;"/>
+               <label for="analog" style="display:inline;">Analog</label>
+            </div>
+            <p></p>
+            <label for="inputindex">input index number :</label>
+            <input id="inputindex" name="inputindex" class="text ui-widget-content ui-corner-all" style="width:165px;">
+         </td>
+
+         <td width="24px">
+         </td>
+
+         <td width="340px" valign="top">
+            <label for="setvalue" style="padding-left:5px;">Value to set :</label>
+            <div id="radio3" style="text-align:left; margin-left:5px; margin-right:5px; padding:10px;" class="ui-widget-content ui-corner-all">
+               <div>
+                  <input type="radio" value=1 class="picker" id="true" name="radio2" checked='true' style="display:inline;"/>
+                  <label for="digital"  style="display:inline;">TRUE</label>
+               </div>
+               <div>
+                  <input type="radio" value=2 class="picker" id="false" name="radio2" style="display:inline;"/>
+                  <label for="analog"  style="display:inline;">FALSE</label>
+               </div>
+               <div>
+                  <input type="radio" value=3 class="picker" id="keyvalue" name="radio2"  style="display:inline;"/>
+                  <label for="keyvalue" style="display:inline;">value from body key :</label>
+                  <input id="key2" name="key2" class="mea_disabled ui-widget-content ui-corner-all" style="text-align:center; display:inline; width:150px;">
+               </div>
+            </div>
+         </td>
+ 
+         </tr>
+      </table>
+ 
+   </div>
+</div>
