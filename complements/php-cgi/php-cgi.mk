7955b90ecc4b8bb71744157aea6becf596b05809
patrice.dietsch <patrice.dietsch@gmail.com>
2014-09-04 15:34:49 +0000

    création makefile pour récupération et compilation d'un php-cgi

--- complements/php-cgi/php-cgi.mk
+++ complements/php-cgi/php-cgi.mk
@@ -1,29 +1,38 @@
-PHP=$SOURCE/complements/php
+ifndef SOURCE
+$(error "SOURCE not set, can't make php-cgi binary")
+endif 
+
+PHPCGI=$SOURCE/complements/php-cgi
+PHPSOURCE=$PHPCGI/src
 DOWNLOADS=$SOURCE/complements/downloads
 
-all: $PHP/php-5.5.16/sapi/cgi/php-cgi
+all: $PHPSOURCE/php-5.5.16/sapi/cgi/php-cgi
 
-$PHP/php-5.5.16/sapi/cgi/php-cgi: $PHP/Makefile
+$PHPSOURCE/php-5.5.16/sapi/cgi/php-cgi: $PHP/Makefile
    cd $PHP/php-5.5.16
    make
 
-$PHP/Makefile: $PHP/php-5.5.16
-   cd $PHP/php-5.5.16
+$PHPSOURCE/Makefile: $PHPSOURCE/php-5.5.16
+   cd $PHPSOURCE/php-5.5.16
    ./configure --prefix=/usr/local/php --disable-all --enable-session --enable-pdo --with-sqlite3 --with-pdo-sqlite --enable-json
 
-$PHP/php-5.5.16: $DOWNLOAD/php5.tar.bz
-   @mkdir -p $PHP
-   cd $PHP
+$PHPSOURCE/php-5.5.16: $DOWNLOAD/php5.tar.bz
+   @mkdir -p $PHPSOURCE
+   cd $PHPSOURCE
    bzip2 -dc $DOWNLOADS/php5.tar.bz | tar xvf -
 
 $DOWNLOAD/php5.tar.bz2:
    @mkdir -p $DOWNLOADS
    curl -silent -L http://fr2.php.net/get/php-5.5.16.tar.bz2/from/this/mirror -o $DOWNLOADS/php5.tar.bz2
 
-install: $PHP/php-5.5.16/sapi/cgi/php-cgi
-   mkdir -p $BUILD/bin
-   cp $PHP/php-5.5.16/sapi/cgi/php-cgi $BUILD/bin
+install: $PHPSOURCE/php-5.5.16/sapi/cgi/php-cgi
+   @mkdir -p $BUILD/bin
+   cp $PHPSOURCE/php-5.5.16/sapi/cgi/php-cgi $BUILD/bin
 
 clean:
-   cd $PHP/php-5.5.16
+   cd $PHPSOURCE/php-5.5.16
    make clean
+
+fullclean:
+   rm -r $PHPSOURCE
+   rm $DOWNLOADS/php5.tar.bz2
