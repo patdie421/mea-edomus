60dc1ce7efa88586fa505639bfc43b9c3c83541b
patrice.dietsch <patrice.dietsch@gmail.com>
2014-09-29 09:43:27 +0000

    contenu de la page index ajouté

---
+++ gui/views/index_contenu.php
@@ -0,0 +1,14 @@
+<?php
+//
+//  SOUS-PAGE (SUB-VIEW) de index.php : statut de fonctionnement de mea-edomus 
+//
+include_once('../lib/configs.php');
+session_start() ?>
+
+<script type="text/javascript" src="lib/js/mea-auth-utils.js"></script>
+<script type="text/javascript" src="lib/js/mea-gui-utils.js"></script>
+
+<script type="text/javascript">
+   jQuery(document).ready(function(){
+   });
+</script> 
