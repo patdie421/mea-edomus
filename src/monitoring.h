1b73d661257f2149fa0435a122890c829e7784e8
patrice.dietsch <patrice.dietsch@gmail.com>
2014-10-01 10:13:57 +0000

    modification des start/stop

--- src/monitoring.h
+++ src/monitoring.h
@@ -9,8 +9,8 @@
 #ifndef mea_edomus_Header_h
 #define mea_edomus_Header_h
 
-pid_t start_nodejs(char *nodejs_path, char *eventServer_path, int port_socketio, int port_socketdata);
-void stop_nodejs();
-void startMonitoringServer(char *nodejs_path, char *eventServer_path, int port_socketio, int port_socketdata, char *log_path);
+
+pthread_t *start_monitoringServer(char *nodejs_path, char *eventServer_path, int port_socketio, int port_socketdata, char *log_path);
+void stop_monitoringServer();
 
 #endif
