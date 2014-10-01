013dc34541d12bbf635a6cb163d58a260ce57757
patrice.dietsch <patrice.dietsch@gmail.com>
2014-10-01 11:29:00 +0000

    start/stop

--- src/interfaces.h
+++ src/interfaces.h
@@ -10,8 +10,10 @@
 
 #include <termios.h>
 #include <inttypes.h>
+#include <sqlite3.h>
 
 #include "types.h"
+#include "queue.h"
 
 typedef struct interfaces_queue_elem_s
 {
@@ -21,5 +23,6 @@ typedef struct interfaces_queue_elem_s
 
 int16_t get_dev_and_speed(char *device, char *dev, int16_t dev_l, speed_t *speed);
 int32_t get_speed_from_speed_t(speed_t speed);
+queue_t *start_interfaces(char **params_list, sqlite3 *sqlite3_param_db);
 
 #endif
