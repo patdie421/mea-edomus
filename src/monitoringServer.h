34081682e48c2b03d17d96e59115cc17e8557a65
patrice.dietsch <patrice.dietsch@gmail.com>
2014-10-02 11:52:07 +0000

    correction

--- src/monitoringServer.h
+++ src/monitoringServer.h
@@ -1,13 +1,13 @@
 //
-//  monitoring.h
+//  monitoringServer.h
 //  mea-edomus
 //
 //  Created by Patrice Dietsch on 25/09/2014.
 //
 //
 
-#ifndef mea_edomus_Header_h
-#define mea_edomus_Header_h
+#ifndef monitoringServer_h
+#define monitoringServer_h
 
 
 //pthread_t *start_monitoringServer(char *nodejs_path, char *eventServer_path, int port_socketio, int port_socketdata, char *log_path);
