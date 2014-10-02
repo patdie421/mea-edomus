4047d167571eeee5f8eaa70ebea65efa4ff993bb
patrice.dietsch <patrice.dietsch@gmail.com>
2014-10-02 11:51:18 +0000

    changement du proto

--- src/monitoringServer.h
+++ src/monitoringServer.h
@@ -10,7 +10,8 @@
 #define mea_edomus_Header_h
 
 
-pthread_t *start_monitoringServer(char *nodejs_path, char *eventServer_path, int port_socketio, int port_socketdata, char *log_path);
+//pthread_t *start_monitoringServer(char *nodejs_path, char *eventServer_path, int port_socketio, int port_socketdata, char *log_path);
+pthread_t *start_monitoringServer(char **parms_list)
 void stop_monitoringServer();
 
 #endif
