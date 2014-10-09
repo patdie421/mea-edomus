9590886814ed9f4f5c52b3bbbad40a78aeb3869b
patrice.dietsch <patrice.dietsch@gmail.com>
2014-10-09 11:36:57 +0000

    iosocket_port

--- src/init.h
+++ src/init.h
@@ -14,7 +14,7 @@
 int16_t create_queries_db(char *queries_db_path);
 int16_t checkInstallationPaths(char *base_path, int16_t try_to_create_flag);
 int16_t checkParamsDb(char *sqlite3_db_param_path, int16_t *cause);
-int16_t create_configs_php(char *gui_home, char *params_db_fullname, char *php_log_fullname, char *php_sessions_fullname);
+int16_t create_configs_php(char *gui_home, char *params_db_fullname, char *php_log_fullname, char *php_sessions_fullname, int iosocket_port);
 int16_t initMeaEdomus(int16_t mode, char **params_list, char **keys);
 int16_t updateMeaEdomus(char **params_list, char **keys);
 
