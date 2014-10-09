54876614a566532c261912b8430a98f503e6b56e
patrice.dietsch <patrice.dietsch@gmail.com>
2014-10-09 14:26:23 +0000

    Ajout des tabs

--- gui/views/index-contenu.php
+++ gui/views/index-contenu.php
@@ -15,22 +15,40 @@ session_start() ?>
 }
 </style>
 
-<div class="ui-corner-all ui-widget-content" style="padding-top: 40px; padding-bottom: 40px;">
-  <div id="console" class="console" style="margin:auto; width:800px; min-height:400px; height:400px;">
-  </div>
-  <div id="info">UNE INFO</div>
+<div id="tabs1">
+<ul>
+<li><a href="#tabs1-1" id='synthese'>Synthèse</a></li>
+<li><a href="#tabs1-2" id='indicateurs'>Indicateurs</a></li>
+<li><a href="#tabs1-3" id='log'>Log</a></li>
+</ul>
+
+<div id="tabs1-1">
 </div>
+
+<div id="tabs1-2">
+   <div id="info">UNE INFO</div>
+</div>
+
+<div id="tabs1-3">
+  <div class="ui-corner-all ui-widget-content" style="padding-top: 40px; padding-bottom: 40px;">
+     <div id="console" class="console" style="margin:auto; min-width:800px; min-height:400px; height:400px;"></div>
+  </div>
+</div>
+
 
 <script type="text/javascript" src="lib/js/mea-auth-utils.js"></script>
 <script type="text/javascript" src="lib/js/mea-gui-utils.js"></script>
-
 <script type="text/javascript" src="controllers/index.js"></script>
+
 <script>
 jQuery(document).ready(function(){
 <?php
    echo "var socketio_port=";
    echo $IOSOCKET_PORT;
    echo ";\n";
-?>
+   ?>
+
+   $( "#tabs1" ).tabs();
    start_index_controller(socketio_port);
+});
 </script>
