54c077b258f4c5906c7960918e2a0a87abf4e891
patrice.dietsch <patrice.dietsch@gmail.com>
2014-10-27 12:48:44 +0000

    ajout du logo

--- gui/views/commun/page-entete.php
+++ gui/views/commun/page-entete.php
@@ -1,5 +1,7 @@
 <div>
-    <div id='logo'  style="width:250px; float:left; height:50px; text-align:left;">LOGO</div>
+    <div id='logo'  style="width:250px; float:left; height:50px; text-align:left;">
+    <img src="lib/logo-mea-eDomus.png" border="0" alt="logo" style="width:250px;"/>
+    </div>
     <div id='pub' style="width:250px; float:right; height:50px; text-align:right;">INFO</div>
     <div id='titre-page' style="height:50px; text-align:center;">CENTRE</div>
 </div>
