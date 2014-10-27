0f79851ed6a02a260bda198aa9652eed0c25bc1d
patrice.dietsch <patrice.dietsch@gmail.com>
2014-10-27 10:52:09 +0000

    Pour liveCom pour tous ...

--- gui/lib/includes.php
+++ gui/lib/includes.php
@@ -8,5 +8,8 @@
 <script type="text/javascript" src="lib/jqGrid/js/i18n/grid.locale-fr.js"></script>
 <script type="text/javascript" src="lib/jqGrid/js/jquery.jqGrid.min.js"></script>
 <script type="text/javascript" src="lib/jqueryFileTree/jqueryFileTree.js"></script>
+<script type="text/javascript" src="lib/noty-2.2.10/js/noty/packaged/jquery.noty.packaged.min.js"></script>
 
 <script type="text/javascript" src="lib/js/mea-gui-utils.js"></script>
+<script type="text/javascript" src="lib/js/mea-livecom-utils.js"></script>
+
