eba8fbe2ab14e9ad2e6da119c40b42b5254f29fd
patrice.dietsch <patrice.dietsch@gmail.com>
2014-10-27 12:52:05 +0000

    logo

--- gui/views/commun/page-entete.php
+++ gui/views/commun/page-entete.php
@@ -1,6 +1,6 @@
 <div>
     <div id='logo'  style="width:250px; float:left; height:50px; text-align:left;">
-    <img src="lib/logo-mea-eDomus.png" border="0" alt="logo" style="height:50px;"/>
+       <img src="../../lib/logo-mea-eDomus.png" border="0" alt="logo" style="height:50px;"/>
     </div>
     <div id='pub' style="width:250px; float:right; height:50px; text-align:right;">INFO</div>
     <div id='titre-page' style="height:50px; text-align:center;">CENTRE</div>
