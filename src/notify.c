64c9de2bbc903523efa2c763bcc9c3e444395893
patrice.dietsch <patrice.dietsch@gmail.com>
2014-10-28 08:02:26 +0000

    Optimisation : pas d'ouverture/fermeture de socket systématique.

--- src/notify.c
+++ src/notify.c
@@ -16,7 +16,7 @@
 #include "sockets_utils.h"
 
 int _port=5600;
-
+int _notify_socket=-1;
 
 void mea_notify_set_port(int p)
 {
@@ -45,7 +45,32 @@ int mea_notify(char *hostname, int port, char *notif_str, char notif_type)
 }
 
 
+int _notify(char *notif_str, char notif_type)
+{
+   if(_notify_socket==-1)
+   {
+      if(mea_socket_connect(&_notify_socket, hostname, port)<0)
+      {
+        _notify_socket==-1;
+        return -1;
+      }
+
+      int notif_str_l=strlen(notif_str)+6;
+      char message[2048];
+      sprintf(message,"$$$%c%cNOT:%c:%s###", (char)(notif_str_l%256), (char)(notif_str_l/256), notif_type, notif_str);
+      ret = mea_socket_send(&_notify_socket, message, notif_str_l+12);
+      if(ret<0)
+      {
+         close(_notify_socket);
+         _notify_socket=-1;
+         return -1;
+      }
+   }
+   return 0;
+}
+
+
 int mea_notify2(char *notif_str, char notif_type)
 {
-   return mea_notify(localhost_const, _port, notif_str, notif_type);
+   return _notify(localhost_const, _port, notif_str, notif_type);
 }
