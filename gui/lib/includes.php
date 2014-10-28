171bd7f942ca5e97fbf6b356182a7769ad154473
patrice.dietsch <patrice.dietsch@gmail.com>
2014-10-28 16:50:38 +0000

    reorg des chargements.

--- gui/lib/includes.php
+++ gui/lib/includes.php
@@ -4,12 +4,11 @@
 <link rel="stylesheet" type="text/css" href="lib/jquery-ui/css/start/jquery-ui-1.10.3.custom.min.css" />
 
 <script type="text/javascript" src="lib/jquery/jquery-1.10.2.js"></script>
+<script type="text/javascript" src="lib/noty-2.2.10/js/noty/packaged/jquery.noty.packaged.min.js"></script>
 <script type="text/javascript" src="lib/jquery-ui/js/jquery-ui-1.10.3.custom.min.js"></script>
+<script type="text/javascript" src="lib/js/mea-livecom-utils.js"></script>
+<script type="text/javascript" src="lib/js/mea-gui-utils.js"></script>
 <script type="text/javascript" src="lib/jqGrid/js/i18n/grid.locale-fr.js"></script>
 <script type="text/javascript" src="lib/jqGrid/js/jquery.jqGrid.min.js"></script>
 <script type="text/javascript" src="lib/jqueryFileTree/jqueryFileTree.js"></script>
-<script type="text/javascript" src="lib/noty-2.2.10/js/noty/packaged/jquery.noty.packaged.min.js"></script>
-
-<script type="text/javascript" src="lib/js/mea-gui-utils.js"></script>
-<script type="text/javascript" src="lib/js/mea-livecom-utils.js"></script>
 
