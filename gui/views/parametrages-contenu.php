96512931ef093271c35177a0faf3fecb739a1524
patrice.dietsch <patrice.dietsch@gmail.com>
2014-10-30 11:08:51 +0000

    liveCom

--- gui/views/parametrages-contenu.php
+++ gui/views/parametrages-contenu.php
@@ -46,74 +46,81 @@ var interfaces_inEdit, sensors_actuators_inEdit, locations_inEdit, types_inEdit,
     isActivate=[false,false,false,false];
 
 jQuery(document).ready(function(){
-    // redimentionnement des grilles
-    function resizeGrid(){
-        active = $("#tabs1").tabs("option", "active");
-        grid=null;
-        switch(active)
-        {
-            case 0: grid='sensors_actuators';
-                    break;
-            case 1: grid='interfaces';
-                    break;
-            case 2: grid='locations';
-                    break;
-            case 3: grid='types';
-                    break;
 
-        }
+<?php
+   echo "var socketio_port=";
+   echo $IOSOCKET_PORT;
+   echo ";\n";
+   ?>
+   liveCom.connect(socketio_port);
+php>
 
-        if(grid){
-            var numTab=active+1;
-            gridParentWidth = $('#gbox_' + 'table_'+grid).parent().width();
-            $('#table_'+grid).jqGrid('setGridWidth',gridParentWidth, false);
-        }
-    }
+   // redimentionnement des grilles
+   function resizeGrid(){
+      active = $("#tabs1").tabs("option", "active");
+      grid=null;
+      switch(active)
+      {
+         case 0: grid='sensors_actuators';
+                 break;
+         case 1: grid='interfaces';
+                 break;
+         case 2: grid='locations';
+                 break;
+         case 3: grid='types';
+                 break;
+      }
+      if(grid){
+         var numTab=active+1;
+         gridParentWidth = $('#gbox_' + 'table_'+grid).parent().width();
+         $('#table_'+grid).jqGrid('setGridWidth',gridParentWidth, false);
+      }
+   }
 
-    // déclenchement des contrôleurs sur selection d'un nouvel onglet
-    function activeGrids(event,ui) {
-        active = $("#tabs1").tabs("option", "active");
+   // déclenchement des contrôleurs sur selection d'un nouvel onglet
+   function activeGrids(event,ui) {
+      active = $("#tabs1").tabs("option", "active");
 
-        switch(active)
-        {
-            case 0: if(!isActivate[active]){
-                        grid_sensors_actuators();
-                    }
-                    else
-                        $('#table_sensors_actuators').trigger( 'reloadGrid' );
-                    break;
-            case 1: if(!isActivate[active]){
-                        grid_interfaces();
-                    }
-                    else
-                        $('#table_interfaces').trigger( 'reloadGrid' );
-                    break;
-            case 2: if(!isActivate[active]){
-                        grid_locations();
-                    }
-                    break;
-            case 3: if(!isActivate[active]){
-                        grid_types();
-                    }
-                    else
-                        $('#table_types').trigger( 'reloadGrid' );
-                    break;
-        }
-        isActivate[active]=true;
-        resizeGrid();
-    }    
+      switch(active)
+      {
+         case 0: if(!isActivate[active]){
+                    grid_sensors_actuators();
+                 }
+                 else
+                    $('#table_sensors_actuators').trigger( 'reloadGrid' );
+                 break;
+         case 1: if(!isActivate[active]){
+                    grid_interfaces();
+                 }
+                 else
+                    $('#table_interfaces').trigger( 'reloadGrid' );
+                 break;
+         case 2: if(!isActivate[active]){
+                    grid_locations();
+                 }
+                 break;
+         case 3: if(!isActivate[active]){
+                    grid_types();
+                 }
+                 else
+                    $('#table_types').trigger( 'reloadGrid' );
+                 break;
+      }
+      isActivate[active]=true;
+      resizeGrid();
+   }    
 
-    // traduction
-    $("#sensors_actuators").text(str_sensors_actuators.capitalize());
-    $("#interfaces").text(str_interfaces.capitalize());
-    $("#locations").text(str_locations.capitalize());
-    $("#types").text(str_types.capitalize());
-    $("#tabs1").tabs({activate:activeGrids});
+   // traduction
+   $("#sensors_actuators").text(str_sensors_actuators.capitalize());
+   $("#interfaces").text(str_interfaces.capitalize());
+   $("#locations").text(str_locations.capitalize());
+   $("#types").text(str_types.capitalize());
+   $("#tabs1").tabs({activate:activeGrids});
 
-    // affichage grille
-    activeGrids();
+   // affichage grille
+   activeGrids();
     
-    // évenements
-    $(window).on('resize',resizeGrid).trigger('resize');
+   // évenements
+   $(window).on('resize',resizeGrid).trigger('resize');
 });
 </script>
