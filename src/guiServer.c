26a8a02413f52df4124ddeaa546d4837703bd448
patrice.dietsch <patrice.dietsch@gmail.com>
2014-11-05 16:32:59 +0000

    ajout d'indicateurs

--- src/guiServer.c
+++ src/guiServer.c
@@ -1,4 +1,4 @@
-  //
+//
 //  httpServer.c
 //  mea-eDomus
 //
@@ -84,6 +84,9 @@ int  _socketdata_port=0;
  NULL
  };
  */
+
+int httpin_indicator=0; // nombre de demandes recues
+int httpout_indicator=0; // nombre de réponses émises
 
 const char *options[15];
 struct mg_context* g_mongooseContext = 0;
@@ -176,6 +179,7 @@ void _httpResponse(struct mg_connection *conn, char *response)
              "\r\n"
              "%s\r\n",
              (int)strlen(response)+2, response);
+   process_update_indicator(_httpServer_monitoring_id, "HTTPOUT", ++httpout_indicator);
 }
 
 
@@ -188,6 +192,7 @@ void _httpErrno(struct mg_connection *conn, int n, char *msg)
       snprintf(errno_str, sizeof(errno_str)-1, "{ \"errno\" : %d }",n);
    
    _httpResponse(conn, errno_str);
+   process_update_indicator(_httpServer_monitoring_id, "HTTPOUT", httpout_indicator++);
 }
 
 
@@ -323,37 +328,6 @@ _phpsessid_check_loggedin_and_admin_clean_exit:
    return ret;
 }
 
-/* old nodejs
-int nodejs_cmnd(char *hostname, int port, char cmnd, char *str)
-{
-   int s;
-   if( mea_socket_connect(&s, hostname, port)==0 )
-   {
-      int str_l=strlen(str)+6;
-      char message[256];
-      snprintf(message,sizeof(message)-8,"$$$%c%cINT:%c:%s###", (char)(str_l%128), (char)(str_l/128), cmnd, str);
-      int ret = mea_socket_send(&s, message, str_l+8);
-      if(ret==0)
-      {
-         int l=mea_socket_read(&s, message, sizeof(message), 1); // avec timeout de 1s
-         message[l]=0;
-         if(l<0)
-         {
-            VERBOSE(1) {
-               fprintf(stderr,"%s (%s) : nodejs not responding.\n", ERROR_STR, __func__);
-            }
-         }
-         close(s);
-      }
-      else
-      {
-         close(s);
-         return -1;
-      }
-   }
-   return -1;
-}
-*/
 
 #define MAX_BUFFER_SIZE 80
 #define MAX_TOKEN 20
@@ -365,6 +339,9 @@ static int _begin_request_handler(struct mg_connection *conn)
    
    char *tokens[MAX_TOKEN]; // 10 tokens maximum
    char buffer[MAX_BUFFER_SIZE];
+
+   httpin_indicator++;
+   process_update_indicator(_httpServer_monitoring_id, "HTTPIN", httpin_indicator);
 
    const char *cookie = mg_get_header(conn, "Cookie");
    if(mg_get_cookie(cookie, "PHPSESSID", phpsessid, sizeof(phpsessid))>0)
@@ -399,7 +376,6 @@ static int _begin_request_handler(struct mg_connection *conn)
       _httpResponse(conn, json);
       return 1;
    }
-
    else if(strlen((char *)request_info->uri)==13 &&  mea_strncmplower("/CMD/ping.php",(char *)request_info->uri,13)==0)
    {
       process_heartbeat(_httpServer_monitoring_id); // le heartbeat est fait de l'extérieur ...
@@ -558,7 +534,6 @@ static int _begin_request_handler(struct mg_connection *conn)
             }
             else
             {
-               //               DEBUG_SECTION fprintf(stderr,"En attente de reponse\n");
                xPL_MessagePtr respMsg=mea_readXPLResponse(id);
                if(respMsg)
                {
@@ -596,68 +571,6 @@ static int _begin_request_handler(struct mg_connection *conn)
    return 0;
 }
 
-/* old nodejs
-void stop_nodejs()
-{
-   int status;
-   
-   if(_pid_nodejs>0)
-   {
-      kill(_pid_nodejs, SIGKILL);
-      wait(&status);
-      _pid_nodejs=-1;
-   }
-   
-   VERBOSE(1) fprintf(stderr,"%s  (%s) : nodejs %s.\n", INFO_STR, __func__,stopped_successfully_str);
-   mea_notify_printf('S', "nodejs %s.",stopped_successfully_str);
-
-}
-*/
-/* oldnodejs
-pid_t start_nodejs(char *nodejs_path, char *eventServer_path, int port_socketio, int port_socketdata, char *phpsession_path)
-{
-   pid_t nodejs_pid = -1;
-   nodejs_pid = fork();
-   
-   if (nodejs_pid == 0) // child
-   {
-      // Code only executed by child process
-      char str_port_socketio[20];
-      char str_port_socketdata[20];
-      char str_phpsession_path[256];
-      
-      char *params[6];
-      
-      sprintf(str_port_socketio,"--iosocketport=%d",port_socketio);
-      sprintf(str_port_socketdata,"--dataport=%d",port_socketdata);
-      sprintf(str_phpsession_path,"--phpsession_path=%s",phpsession_path);
-      
-      params[0]="mea-edomus[nodejs]";
-      params[1]=eventServer_path;
-      params[2]=str_port_socketio;
-      params[3]=str_port_socketdata;
-      params[4]=str_phpsession_path;
-      params[5]=0;
-      
-      execvp(nodejs_path, params);
-
-      VERBOSE(1) fprintf(stderr,"%s (%s) : can't start nodejs Server (execvp).\n", ERROR_STR, __func__);
-      perror("");
-      
-      exit(1);
-   }
-   else if (nodejs_pid < 0)
-   { // failed to fork
-      VERBOSE(1) fprintf(stderr,"%s (%s) : can't start nodejs server (fork).\n", ERROR_STR, __func__);
-      perror("");
-      return -1;
-   }
-   // Code only executed by parent process
-   VERBOSE(1) fprintf(stderr,"%s  (%s) : nodejs server %s.\n", INFO_STR, __func__, launched_successfully_str);
-   mea_notify_printf('S', "nodejs server %sy.", launched_successfully_str);
-   return nodejs_pid;
-}
-*/
 
 int stop_httpServer()
 {
@@ -708,6 +621,9 @@ mea_error_t start_httpServer(uint16_t port, char *home, char *php_cgi, char *php
    callbacks.begin_request = _begin_request_handler;
    //   callbacks.open_file = open_file_handler;
    
+   process_add_indicator(_httpServer_monitoring_id, "HTTPIN", httpin_indicator);
+   process_add_indicator(_httpServer_monitoring_id, "HTTPOUT", httpout_indicator);
+
    g_mongooseContext = mg_start(&callbacks, NULL, options);
    if (g_mongooseContext == NULL)
       return ERROR;
