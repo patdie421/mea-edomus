2cb0798651bff7cc88f3872a04ee636cb7e6cb2d
patrice.dietsch <patrice.dietsch@gmail.com>
2014-11-10 13:57:05 +0000

    upgrade paramsdb

--- src/init.h
+++ src/init.h
@@ -11,12 +11,17 @@
 
 #include <inttypes.h>
 
+struct upgrade_params_s
+{
+   *params_list[];
+}
+
 int16_t create_queries_db(char *queries_db_path);
 int16_t checkInstallationPaths(char *base_path, int16_t try_to_create_flag);
 int16_t checkParamsDb(char *sqlite3_db_param_path, int16_t *cause);
 int16_t create_configs_php(char *gui_home, char *params_db_fullname, char *php_log_fullname, char *php_sessions_fullname, int iosocket_port);
 int16_t initMeaEdomus(int16_t mode, char **params_list, char **keys);
 int16_t updateMeaEdomus(char **params_list, char **keys);
-int16_t upgrade_params_db(uint16_t fromVersion, uint16_t toVersion);
+int16_t upgrade_params_db(uint16_t fromVersion, uint16_t toVersion, struct upgrade_params_s *upgrade_params);
 
 #endif
