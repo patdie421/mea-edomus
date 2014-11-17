2a86748588871f23d6a7814bb0ea9d58f0320b74
patrice.dietsch <patrice.dietsch@gmail.com>
2014-11-17 11:12:28 +0000

    optimisation

--- src/logServer.h
+++ src/logServer.h
@@ -15,6 +15,9 @@ struct logServerData_s
 };
 
 extern char *log_server_name_str;
+extern char *log_server_logsenterr_str;
+extern char *log_server_readerror_str;
+extern char *log_server_logsenterr_str;
 
 int start_logServer(int my_id, void *data, char *errmsg, int l_errmsg);
 int stop_logServer(int my_id, void *data, char *errmsg, int l_errmsg);
