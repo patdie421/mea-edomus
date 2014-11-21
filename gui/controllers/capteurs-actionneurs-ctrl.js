d9a6386ae04daa2d6f52196afae55f2720648e86
patrice.dietsch <patrice.dietsch@gmail.com>
2014-11-21 14:26:56 +0000

    Traitement d'erreur

--- gui/controllers/capteurs-actionneurs-ctrl.js
+++ gui/controllers/capteurs-actionneurs-ctrl.js
@@ -3,9 +3,12 @@ function capteursactionneurs_controller(table_id)
    console.log($("#"+table_id).text());
    
    $('#'+table_id).edatagrid({
-      url: 'models/get_sensorsactuators-jqg_grid.php',
-      saveUrl: 'save_user.php',
-      updateUrl: 'update_user.php',
-      destroyUrl: 'destroy_user.php'
+      url:        'models/get_sensorsactuators-jqg_grid.php',
+      saveUrl:    'save_user.php',
+      updateUrl:  'update_user.php',
+      destroyUrl: 'destroy_user.php',
+      onError:    function(index,row) { alert(row.msg); }
    });
+
+
 }
