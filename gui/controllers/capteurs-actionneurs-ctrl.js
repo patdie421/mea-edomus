09e814f9edbe869090f73335a9a4bb23bb710fde
patrice.dietsch <patrice.dietsch@gmail.com>
2014-11-21 14:35:44 +0000

    Correction

--- gui/controllers/capteurs-actionneurs-ctrl.js
+++ gui/controllers/capteurs-actionneurs-ctrl.js
@@ -1,7 +1,7 @@
 function capteursactionneurs_controller(table_id)
 {
    console.log($("#"+table_id).text());
-   
+/*   
    $('#'+table_id).edatagrid({
       url:        'models/get_sensorsactuators-jqg_grid.php',
       saveUrl:    'save_user.php',
@@ -9,4 +9,5 @@ function capteursactionneurs_controller(table_id)
       destroyUrl: 'destroy_user.php',
       onError:    function(index,row) { alert(row.msg); } // uniquement pour edatagrid ?
    });
+*/
 }
