31339cb1438ff4a0117dba65e42d8c9189869ec3
patrice.dietsch <patrice.dietsch@gmail.com>
2014-11-24 10:46:54 +0000

    Correction

--- gui/controllers/page2-ctrl.js
+++ gui/controllers/page2-ctrl.js
@@ -1,10 +1,10 @@
 var isadmin=-1;
 
-function page2_controller()
+function page2_controller(tabName)
 {
    authdata=get_auth_data();
    if(authdata==false) {
-      $.messager.alert(str_Error+str_double_dot,str_not_connected,'error', function(){window.location = "login.php";});
+      $.messager.alert(str_Error+str_double_dot,str_not_connected,'error', function(){window.location = "login.php?dest=index.html&page=page2.php&tab="+tabName;});
       return false;
    } else {
       if(authdata.profil!=1) {
@@ -12,6 +12,7 @@ function page2_controller()
       } else {
          isadmin=1;
       }
+      $('#tabConfiguration').tabs('select', tabName);
    }
 }
 
