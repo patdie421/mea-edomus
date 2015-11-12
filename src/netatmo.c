7d857edaaef98a00fba9bfd03c33c5f9ac4c2950
Patrice Dietsch <patrice.dietsch@gmail.com>
2015-11-12 12:05:14 +0100

    suite intégration NETATMO

--- src/netatmo.c
+++ src/netatmo.c
@@ -3,32 +3,45 @@
 #include <unistd.h>
 #include <string.h>
 
+#include "netatmo.h"
 #include <curl/curl.h>
 #include "cJSON.h"
-
 #include "curl_adds.h"
-
 #include "mea_verbose.h"
 
 
-char *token_test="{\"access_token\":\"563de4bb2baa3cbb89752192|1eaa34f6cb5e7b56db2119d457368a19\",\"refresh_token\":\"563de4bb2baa3cbb89752192|4af35da41620a0f30af806c1d83872fe\",\"scope\":[\"read_thermostat\",\"write_thermostat\"],\"expires_in\":10800,\"expire_in\":10800}";
-
-
-struct netatmo_token_s
+static int _netatmo_parse_return_json(char *response, char *err, int l_err)
 {
-   char access[81];
-   char refresh[81]; 
-   time_t expire_in;
-};
+   int ret_code=-1;
 
+   cJSON *response_cjson=cJSON_Parse(response);
+   if(response_cjson==NULL)
+   {
+       DEBUG_SECTION mea_log_printf("%s (%s) : JSON error\n", DEBUG_STR, __func__);
+       goto _netatmo_parse_return_json_clean_exit;
+   }
 
-struct netatmo_thermostat_data_s
-{
-   int battery_vp;
-   int therm_relay_cmd;
-   double temperature;
-   double setpoint_temp;
-};
+   cJSON *e=cJSON_GetObjectItem(response_cjson, "status");
+   if(e && e->type==cJSON_String)
+   {
+      if(strcmp(e->valuestring, "ok")==0)
+         ret_code=0;
+      else
+      {
+         if(err)
+            strncpy(err, e->valuestring, l_err);
+         ret_code=1;
+      }
+   } 
+
+_netatmo_parse_return_json_clean_exit:
+   if(response_cjson)
+   {
+      cJSON_Delete(response_cjson);
+      response_cjson=NULL;
+   }
+   return ret_code;
+}
 
 
 static int _netatmo_parse_data_json(char *response, char *thermostat_id, struct netatmo_thermostat_data_s *thermostat_data,  char *err, int l_err)
@@ -44,27 +57,26 @@ static int _netatmo_parse_data_json(char *response, char *thermostat_id, struct 
 
    cJSON *e = NULL;
 
-    e=cJSON_GetObjectItem(response_cjson, "error");
-    if(e)
-    {
-       cJSON *a=cJSON_GetObjectItem(e, "message");
-       if(a)
-       {
-          DEBUG_SECTION mea_log_printf("%s (%s) : NETATMO error : %s\n", DEBUG_STR, __func__, a->valuestring);
-          if(err)
-             strncpy(err, a->valuestring, l_err);
-       }
-       else
-       {
-          if(err)
-             strncpy(err,"unknown error", l_err);
-       }
-       a=cJSON_GetObjectItem(e, "code");
-       if(a)
-          ret_code=a->valueint;
-       else
-          ret_code=9999;
-       goto _netatmo_parse_data_json_clean_exit;
+   e=cJSON_GetObjectItem(response_cjson, "error");
+   if(e)
+   {
+      cJSON *a=cJSON_GetObjectItem(e, "message");
+      if(a)
+      {
+         if(err)
+            strncpy(err, a->valuestring, l_err);
+      }
+      else
+      {
+         if(err)
+            strncpy(err,"unknown error", l_err);
+      }
+      a=cJSON_GetObjectItem(e, "code");
+      if(a)
+         ret_code=a->valueint;
+      else
+         ret_code=9999;
+      goto _netatmo_parse_data_json_clean_exit;
    }
 
    e=cJSON_GetObjectItem(response_cjson, "body"); 
@@ -106,22 +118,46 @@ static int _netatmo_parse_data_json(char *response, char *thermostat_id, struct 
                goto _netatmo_parse_data_json_clean_exit;
             thermostat_data->therm_relay_cmd=e->valueint;
 
+            e=cJSON_GetObjectItem(a, "setpoint");
+            if(e==NULL)
+               goto _netatmo_parse_data_json_clean_exit;
+            {
+               char *s=cJSON_GetObjectItem(e, "setpoint_mode")->valuestring;
+               if(s==NULL)
+                  goto _netatmo_parse_data_json_clean_exit;
+               if(strcmp(s,"program")==0)
+                  thermostat_data->setpoint=PROGRAM;
+               else if(strcmp(s, "away")==0)
+                  thermostat_data->setpoint=AWAY;
+               else if(strcmp(s,"hg")==0)
+                  thermostat_data->setpoint=HG;
+               else if(strcmp(s,"manual")==0)
+                  thermostat_data->setpoint=MANUAL;
+               else if(strcmp(s,"off")==0)
+                  thermostat_data->setpoint=OFF;
+               else if(strcmp(s,"max")==0)
+                  thermostat_data->setpoint=MAX;
+               else
+                  thermostat_data->setpoint=-1; 
+            }
 
             cJSON *mesured=cJSON_GetObjectItem(a, "measured");
             if(mesured==NULL)
                goto _netatmo_parse_data_json_clean_exit;
+            else
+            {
+               e=cJSON_GetObjectItem(mesured, "temperature");
+               if(e==NULL)
+                  goto _netatmo_parse_data_json_clean_exit;
+               thermostat_data->temperature=e->valuedouble;
 
-            e=cJSON_GetObjectItem(mesured, "temperature");
-            if(e==NULL)
-               goto _netatmo_parse_data_json_clean_exit;
-            thermostat_data->temperature=e->valuedouble;
-
-            e=cJSON_GetObjectItem(mesured, "setpoint_temp");
-            if(e==NULL)
-               goto _netatmo_parse_data_json_clean_exit;
-             thermostat_data->setpoint_temp=e->valuedouble;
-            ret_code=0;
-            break;
+               e=cJSON_GetObjectItem(mesured, "setpoint_temp");
+               if(e==NULL)
+                  goto _netatmo_parse_data_json_clean_exit;
+               thermostat_data->setpoint_temp=e->valuedouble;
+               ret_code=0;
+               break;
+            }
          }
          i++;
       }
@@ -204,12 +240,69 @@ netatmo_parse_token_json_clean_exit:
 }
 
 
+int netatmo_refresh_token(char *client_id, char *client_secret, struct netatmo_token_s *netatmo_token, char *err, int l_err)
+{
+   CURL *curl;
+   char *url="https://api.netatmo.net/oauth2/token";
+   int ret_code=-1;
+   char post_data[1024];
+
+   if(netatmo_token->access[0]==0 || netatmo_token->refresh[0]==0)
+   {
+      DEBUG_SECTION mea_log_printf("%s (%s) : token not initialized (%d)\n", DEBUG_STR, __func__);
+      return -1;
+   }
+
+   sprintf(post_data, "grant_type=refresh_token&client_id=%s&client_secret=%s&refresh_token=%s", client_id, client_secret, netatmo_token->refresh);
+
+   struct curl_result_s cr;
+   int ret=-1;
+
+   ret=curl_result_init(&cr);
+   if(ret<0)
+      return -1;
+
+   curl = curl_easy_init();
+   if(curl)
+   {
+      curl_easy_setopt(curl, CURLOPT_URL, url);
+
+//      DEBUG_SECTION curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+      curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_data);
+      curl_easy_setopt(curl, CURLOPT_TIMEOUT, 30L);
+      curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, (curl_write_callback)curl_result_get);
+      curl_easy_setopt(curl, CURLOPT_WRITEDATA, &cr);
+
+      CURLcode res = curl_easy_perform(curl);
+      if(res != CURLE_OK)
+      {
+         DEBUG_SECTION mea_log_printf("%s (%s) : curl_easy_perform() failed (%d) - %s\n", DEBUG_STR, __func__, time(NULL), curl_easy_strerror(res));
+         ret=-1;
+      }
+      else
+         ret=_netatmo_parse_token_json(cr.p, netatmo_token, err, l_err);
+
+      curl_easy_cleanup(curl);
+   }
+   else
+      ret=-1;
+
+   curl_result_release(&cr);
+
+   return ret;
+}
+
+
 int netatmo_get_token(char *client_id, char *client_secret, char *username, char *password, char *scope, struct netatmo_token_s *netatmo_token, char *err, int l_err)
 {
    CURL *curl;
    char *url="https://api.netatmo.net/oauth2/token";
    int ret_code=-1;
-   char post_data[255];
+   char post_data[1024];
 
    sprintf(post_data, "grant_type=password&client_id=%s&client_secret=%s&username=%s&password=%s&scope=%s", client_id,client_secret,username,password,scope);
      
@@ -225,7 +318,7 @@ int netatmo_get_token(char *client_id, char *client_secret, char *username, char
    {
       curl_easy_setopt(curl, CURLOPT_URL, url);
 
-      curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+//      DEBUG_SECTION curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
       curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
       curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
       curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
@@ -234,8 +327,6 @@ int netatmo_get_token(char *client_id, char *client_secret, char *username, char
       curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
       curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, (curl_write_callback)curl_result_get);
       curl_easy_setopt(curl, CURLOPT_WRITEDATA, &cr);
-
-      DEBUG_SECTION mea_log_printf("%s (%s) : try authentification ... (%d)\n", DEBUG_STR, __func__,time(NULL));
 
       CURLcode res = curl_easy_perform(curl);
       if(res != CURLE_OK)
@@ -257,14 +348,114 @@ int netatmo_get_token(char *client_id, char *client_secret, char *username, char
 }
 
 
-int netatmo_get_data(char *access_token, char *relay_id, char *thermostat_id, struct netatmo_thermostat_data_s *thermostat_data, char *err, int l_err)
+int netatmo_set_thermostat_setpoint(char *access_token, char *relay_id, char *thermostat_id, enum netatmo_setpoint_mode_e mode, uint32_t delay, double temp, char *err, int l_err)
 {
    CURL *curl;
-   char *_url="https://api.netatmo.net/api/getthermostatsdata";
+   char *api="https://api.netatmo.net/api/setthermpoint";
+   char *_mode=NULL;
+   char post_data[1024];
+   time_t endtime=0;
+   double _temp=0.0;
 
-   char url[255];
+   if(delay!=0)
+      endtime=time(NULL)+delay;
 
-   sprintf(url, "%s?access_token=%s&device_id=%s", _url, access_token, relay_id);
+   switch(mode)
+   {
+      case PROGRAM:
+         _mode="program";
+         break;
+      case AWAY:
+         _mode="away";
+         break;
+      case HG:
+         _mode="hg";
+         break;
+      case MANUAL:
+         _mode="manual";
+         if(temp<7.0)
+         {
+            if(err)
+               snprintf(err,l_err,"temp (%3.0f °C) too low !\n", temp);
+            return 1;
+         }
+         _temp=temp;
+         break;
+      case OFF:
+         _mode="off";
+         break;
+      case MAX:
+         _mode="max";
+         if(delay==0)
+            delay=3600;
+         endtime=time(NULL)+delay;
+         break;
+      default:
+         return -1;
+   }
+
+   sprintf(post_data, "access_token=%s&device_id=%s&module_id=%s&setpoint_mode=%s", access_token, relay_id, thermostat_id, _mode);
+   if(_temp!=0.0)
+   {
+      char spt[5];
+      sprintf(spt, "%04.1f", _temp);
+      mea_snprintfcat(post_data, sizeof(post_data), "&setpoint_temp=%s", spt); 
+   }
+   if(endtime!=0)
+      mea_snprintfcat(post_data, sizeof(post_data), "&setpoint_endtime=%d", endtime);
+
+     struct curl_result_s cr;
+   int ret=-1;
+
+   ret=curl_result_init(&cr);
+   if(ret<0)
+      return -1;
+
+   curl = curl_easy_init();
+   if(curl)
+   {
+      curl_easy_setopt(curl, CURLOPT_URL, api);
+
+//      DEBUG_SECTION curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+      curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_data);
+      curl_easy_setopt(curl, CURLOPT_TIMEOUT, 30L);
+      curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, (curl_write_callback)curl_result_get);
+      curl_easy_setopt(curl, CURLOPT_WRITEDATA, &cr);
+
+      CURLcode res = curl_easy_perform(curl);
+      if(res != CURLE_OK)
+      {
+         DEBUG_SECTION mea_log_printf("%s (%s) : curl_easy_perform() failed (%d) - %s\n", DEBUG_STR, __func__, time(NULL), curl_easy_strerror(res));
+         ret=-1;
+      }
+      else
+      {
+         ret=_netatmo_parse_return_json(cr.p, err, l_err);
+      }
+
+      curl_easy_cleanup(curl);
+   }
+   else
+      ret=-1;
+
+   curl_result_release(&cr);
+
+   return ret;
+}
+
+
+int netatmo_get_thermostat_data(char *access_token, char *relay_id, char *thermostat_id, struct netatmo_thermostat_data_s *thermostat_data, char *err, int l_err)
+{
+   CURL *curl;
+   char *api="https://api.netatmo.net/api/getthermostatsdata";
+
+   char url[1024];
+
+   sprintf(url, "%s?access_token=%s&device_id=%s", api, access_token, relay_id);
      
    struct curl_result_s cr;
    int ret=-1;
@@ -278,7 +469,7 @@ int netatmo_get_data(char *access_token, char *relay_id, char *thermostat_id, st
    {
       curl_easy_setopt(curl, CURLOPT_URL, url);
 
-//      curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+//      DEBUG_SECTION curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
       curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
       curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
       curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
@@ -306,19 +497,45 @@ int netatmo_get_data(char *access_token, char *relay_id, char *thermostat_id, st
 }
 
 
+#ifdef NETATMO_MODULE_TEST
 int main(int argc, char *argv[])
 {
    struct netatmo_token_s token;
    struct netatmo_thermostat_data_s data;
    char err[80];
+   int ret;
 
-//   netatmo_get_token("563e5ce3cce37c07407522f2","lE1CUF1k3TxxSceiPpmIGY8QXJWIeXJv0tjbTRproMy4","patrice.dietsch@gmail.com","WEBcdpii10","read_thermostat write_thermostat", &token, err, sizeof(err)-1);
+   ret=netatmo_get_token("563e5ce3cce37c07407522f2","lE1CUF1k3TxxSceiPpmIGY8QXJWIeXJv0tjbTRproMy4","patrice.dietsch@gmail.com","WEBcdpii10", "read_thermostat write_thermostat", &token, err, sizeof(err)-1);
+   if(ret!=0)
+   {
+      if(ret<0)
+         printf("Authentification error : %d\n", ret);
+      else
+         printf("Authentification error : %s (%d)\n", err, ret);
+      exit(1);
+   }
 
-   int ret=netatmo_get_data("563de4bb2baa3cbb89752192|218d344476bf32f17ebcb08f685c31d3", "70:ee:50:0a:34:e0", "04:00:00:0a:37:8c", &data, err, sizeof(err)-1);
 
+   ret=netatmo_refresh_token("563e5ce3cce37c07407522f2","lE1CUF1k3TxxSceiPpmIGY8QXJWIeXJv0tjbTRproMy4", &token, err, sizeof(err)-1);
+   if(ret!=0)
+   {
+      if(ret<0)
+         printf("Refresh error : %d\n", ret);
+      else
+         printf("Refresh error : %s (%d)\n", err, ret);
+      exit(1);
+   }
+
+
+   ret=netatmo_get_thermostat_data(token.access, "70:ee:50:0a:34:e0", "04:00:00:0a:37:8c", &data, err, sizeof(err)-1);
    if(ret==0)
-      printf("temperature = %f\n", data.temperature);
+      printf("temperature = %f %d\n", data.temperature,data.setpoint);
    else if(ret>0)
       printf("error : %s (%d)\n", err, ret); 
+
+//   netatmo_set_thermostat_setpoint(token.access, "70:ee:50:0a:34:e0", "04:00:00:0a:37:8c", MANUAL, 3600, 19.5, err, sizeof(err)-1);
+//   netatmo_set_thermostat_setpoint(token.access, "70:ee:50:0a:34:e0", "04:00:00:0a:37:8c", MAX, 3600, -1.0, err, sizeof(err)-1);
+   netatmo_set_thermostat_setpoint(token.access, "70:ee:50:0a:34:e0", "04:00:00:0a:37:8c", AWAY, 3600, -1.0, err, sizeof(err)-1);
 }
+#endif
 
---
+++ src/netatmo.h
@@ -0,0 +1,38 @@
+#ifndef __netatmo_h
+#define __netatmo_h
+
+#include <inttypes.h>
+
+enum netatmo_setpoint_mode_e
+{
+   PROGRAM,
+   AWAY,
+   HG,
+   MANUAL,
+   OFF,
+   MAX
+};
+
+struct netatmo_token_s
+{
+   char access[81];
+   char refresh[81]; 
+   time_t expire_in;
+};
+
+struct netatmo_thermostat_data_s
+{
+   int battery_vp;
+   int therm_relay_cmd;
+   double temperature;
+   enum netatmo_setpoint_mode_e setpoint;
+   double setpoint_temp;
+};
+
+int netatmo_get_token(char *client_id, char *client_secret, char *username, char *password, char *scope, struct netatmo_token_s *netatmo_token, char *err, int l_err);
+int netatmo_refresh_token(char *client_id, char *client_secret, struct netatmo_token_s *netatmo_token, char *err, int l_err);
+int netatmo_set_thermostat_setpoint(char *access_token, char *relay_id, char *thermostat_id, enum netatmo_setpoint_mode_e mode, uint32_t delay, double temp, char *err, int l_err);
+int netatmo_get_thermostat_data(char *access_token, char *relay_id, char *thermostat_id, struct netatmo_thermostat_data_s *thermostat_data, char *err, int l_err);
+
+#endif
+
