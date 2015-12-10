391ccc23ce9dcd9e1d7c6efd7f0024b0aeeb1cef
Patrice Dietsch <patrice.dietsch@gmail.com>
2015-12-10 20:15:16 +0100

    eval

---
+++ src/mea_eval.h
@@ -0,0 +1,28 @@
+//
+//  mea_eval.h
+//
+//  Created by Patrice DIETSCH on 10/12/15.
+//
+//
+#define DIRECTINTERP 1
+
+typedef int (*getVarVal_f)(int id, void *userdata, double *d);
+typedef int (*getVarId_f)(char *str, void *userdata, double *d);
+
+struct eval_stack_s {
+   char type;
+   union {
+      int op;
+      double value;
+      int id;
+   } val;
+};
+
+int setGetVarCallBacks(getVarId_f fid, getVarVal_f fval, void *userdata);
+
+#if DIRECTINTERP==0
+struct eval_stack_s *getEvalStack(char *str, char **p, int *err, int *stack_ptr);
+int evalCalc(struct eval_stack_s *stack, int stack_ptr, double *r);
+#else
+int evalCalc(char *str, char **p, double *r, int *err);
+#endif
